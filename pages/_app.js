e3833d3f61a8b59a594cf62e1d92a73a7df0b321
Sarah <sarah.saker@hotmail.fr>
2024-08-04 17:21:19 +0200

    Front React + Redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: { search },
+});
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
-        <title>Next.js App</title>
+        <title>Kairos</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
