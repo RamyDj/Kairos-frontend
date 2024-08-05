8d085614c35f4662fd6a8d8c5eb1baf306a13181
Irene <gallibour.irene@gmail.com>
2024-08-05 13:01:57 +0200

    irene : composant signin signup et login + reducer users

--- components/Login.js
+++ components/Login.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
 // import styles from '../styles/Login.module.css';
 
 import SignUp from './SignUp.js';
-// import SignIn from './SignIn.js';
+import SignIn from './SignIn.js';
 
 function Login() {
   return (
     <div>
         <h1>LOGIN</h1>
         <SignUp />
+        <SignIn />
     </div>
   );
 }
--- components/SignIn.js
+++ components/SignIn.js
@@ -1,12 +1,75 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { login } from '../reducers/user';
-import Image from 'next/image';
-import styles from '../styles/SignUp.module.css';
+
+import styles from '../styles/SignIn.module.css';
 
 function SignIn() {
+    const [mail, setMail] = useState('')
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState(false)
+    const [passwordFormatError, setPasswordFormatError] = useState(false)
 
+    const url = process.env.NEXT_PUBLIC_BACK_ADDRESS
+
+    //Reducer 
+    const dispatch = useDispatch();
+
+    // Page Redirection 
+    const router = useRouter();
+
+    const handleSubmit = () => {
+        const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            // const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8}$/; => // Tout decomenter une fois les test ok
+
+        // Reset errors 
+        setEmailError(false);
+            // setPasswordFormatError(false);
+    
+        // Validate password and email format
+        if (!EMAIL_REGEX.test(mail)) {
+            setEmailError(true);
+            return;
+        }
+        /*if (!PASSWORD_REGEX.test(password)) {
+            setPasswordFormatError(true);
+        }*/
+
+            fetch(`${url}/users/signin`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: mail, password }),
+            }).then(response => response.json())
+            .then(data => {
+                data.result && dispatch(login({token: data.token, email: data.user.email }))
+                router.push('/dashboard')
+            })
+      }
+    return (
+        <div className={styles.container}>
+          <h3 className={styles.title}>Connexion</h3>
+            <div className={styles.buttonGoogle}>
+                <button className={styles.button}>Se connecter avec Google</button> 
+            </div>
+            <div className={styles.connexion}>
+                <input 
+                    type="email" 
+                    className={styles.input} 
+                    onChange={(e) => setMail(e.target.value)} 
+                    value={mail} 
+                    placeholder="Adresse mail" />
+                    {emailError && <p style={{ color: 'red' }}>Email non conforme</p>}
+                <input 
+                    type="password" 
+                    className={styles.input} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    value={password} 
+                    placeholder="Mot de passe" />
+            </div>
+          <button className={styles.button} onClick={() => handleSubmit()}>Je me connecte</button>
+        </div>
+      )
 }
 
 export default SignIn;
