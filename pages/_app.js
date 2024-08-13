47b0d85ad3fa839534f232d8d1410c82ac4c2700
raphaelle <raflanelle@hotmail.com>
2024-08-13 16:35:32 +0200

    persist-redux raf

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,37 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import user from '../reducers/user';
 import search from '../reducers/search'
 import Header from '../components/Header/Header';
 
+//redux-persist imports
+import {persistStore, persistReducer} from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({user, search});
+ const persistConfig = {key: 'AppliTest', storage};
+
 const store = configureStore({
-  reducer: { user,search },
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
 });
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (      
 
       <Provider store={store}>
+        <PersistGate persistor={persistor}>
         <Head>
           <title>Kairos</title>
         </Head>
         {<Header />}
         <Component {...pageProps} />
+        </PersistGate>
       </Provider>
 
   );
