c9c673433e8e212aeeeb088782c89b49f5e1f8cd
Jul1enF <j.furic@gmail.com>
2024-08-14 16:15:13 +0200

    Julien Adapatation tableau et phrase responsive LastSearch

--- components/Dashboard/LastSearch.js
+++ components/Dashboard/LastSearch.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import GraphDashboard from './GraphDashboard';
 import styles from '../../styles/Dashboard.module.css';
 import Link from 'next/link';
+const moment = require('moment')
 
 function LastSearch() {
     const user = useSelector((state) => state.user.value);
@@ -16,16 +17,17 @@ function LastSearch() {
             </div>
         )
     }
-
+    const date = moment(search[i.date]).format('DD-MM-YYYY')
     return (
         <div className={styles.lastSearchContainer}>
             <GraphDashboard />
-            <ul>
-                <il>Recherché le <span>06/08/2024</span></il><br/>
-                <il>Activité<span>Coiffeur- visagiste</span></il><br/>
-                <il>Secteur Geographique<span>Nantes</span></il><br/>
-                <il><Link href="/result/companies">Accéder à ma dernière recherche</Link></il>
-            </ul>
+            <div className={styles.searchDescription}>
+                <p>Recherché le : {date}</p>
+                <p>Activité : {search[i].activity}</p>
+                <p>Secteur Géographique : {search[i].area}</p>
+                <Link href="/result/companies"><p className={styles.linkLastSearch}>Accéder à ma dernière recherche</p></Link>
+            </div>
+
             <div className={styles.LastSearchLink}>
                 
             </div>
