1485bb3eb7e053d360ca82cd96109a1f53adb645
Sarah <sarah.saker@hotmail.fr>
2024-08-14 05:01:49 +0200

    Sarah : modif result pour intégrer score (en cours)

--- components/Result/Result.js
+++ components/Result/Result.js
@@ -2,52 +2,38 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fillWithAllUserSearches } from '../../reducers/search';
 import { addIdOfASearch, fillSearchesWithAllId} from '../../reducers/user';
 import styles from '../../styles/Result.module.css';
+import resultStyles from '../../styles/Result.module.css'; 
 import dynamic from 'next/dynamic';
 import Graph from '../../components/Result/Graph';
 import Show from '../../components/Result/Show';
-import Histogram from '../../components/Result/Histogram';
-import Link from 'next/link';
+import Status from '../../components/Result/Status';
 import { useEffect } from 'react';
-import { useRouter } from 'next/router'
+
 
 
 const MapComponent = dynamic(() => import('../../components/Result/MapComponent'), { ssr: false });
 
 function Result() {
-    
+    const search = useSelector((state) => state.search.value);
     const user = useSelector((state) => state.user.value);
     const dispatch = useDispatch()
+   
     const url = process.env.NEXT_PUBLIC_BACK_ADDRESS
-
-    let search
-    const router = useRouter()
-    const {searchid} = router.query
-
-    const allSearches = useSelector((state)=>state.search.value)
-
-    if (searchid !== "companies"){
-    search = allSearches.filter(e=>e._id== searchid)
-    }
-    else {
-    search = allSearches
-    }
 
     console.log(user)
     console.log(search)
-    console.log(allSearches)
 
-
-    useEffect(()=>{
-    const i = allSearches.length-1
+useEffect(()=>{
+    const i = search.length-1
 
     // Si un utilisateur arrive sur la page avec un token et une recherche non enregistrée, fetch de la route pour enregistrer celle ci
     
-    if (user.token && allSearches[i]!=="Aucune entreprise trouvée pour ce type d'activité dans ce secteur." && !allSearches[i]._id)
+    if (user.token && !search[i]._id)
     {
         fetch(`${url}/results/registerSearch`,{
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({search : allSearches[i], email : user.email })
+            body: JSON.stringify({search : search[i], email : user.email })
             })
             .then(response=>response.json())
             .then(data=>{
@@ -57,34 +43,28 @@ function Result() {
     }
 },[])
 
-const i = search.length-1
+const score = search[0].score.average_ca + search[0].score.average_lifetime + search[0].score.density_of_companies + search[0].score.turnover;
+console.log(score)
 
 let scoreStyle;
  
-    if (search[i].score < 50) {
-        scoreStyle = { 'color': '#CF0506' };
+     if (score < 50) {
+        scoreStyle = {'color' : '#CF0506'};
     }
-    else if (search[i].score >= 50 && search.score < 75) {
-        scoreStyle = { 'color': '#FD5C0D' };
+    else if (score >= 50 && score < 75) {
+        scoreStyle = {'color' : '#FD5C0D'};
     }
     else {
-        scoreStyle = { 'color': '#1E8F28' };
-    }
-    const histogramData = search[i].top_status.map(status => ({
-        status_name: status.status_name,
-        companies_per_year: [
-            { year: '2022', number: status.companies_per_year[2]?.number || 0 },
-            { year: '2023', number: status.companies_per_year[1]?.number || 0 },
-            { year: '2024', number: status.companies_per_year[0]?.number || 0 }
-        ]
-    }));
+        scoreStyle = {'color' : '#1E8F28'};
+    }  
 
+      
     return (
         <div>
             <div className={styles.scoreContainer}>
                 <p className={styles.score}
-                >Score :<span style={scoreStyle}
-                > {search[i].score}/100</span></p>
+                >Score :<span style={scoreStyle} 
+                > {score}/100</span></p>
                 <span className={styles.index}
                 >Indice de viabilité</span>
             </div>
@@ -92,7 +72,7 @@ let scoreStyle;
                 <div className={styles.mapResult}>
                     <MapComponent />
                     <span className={styles.companiesNb}
-                    > Nombre d'entreprises : {search[i].current_companies.length}</span>
+                    > Nombre d'entreprises : {search[0].current_companies.length}</span>
                 </div>
                 <div className={styles.showResult}>
                     <Show />
@@ -100,16 +80,15 @@ let scoreStyle;
             </div>
 
             <div className={styles.detailledResult}>
-                <h3>DETAILS DE MA RECHERCHE</h3>
+                <h3>STATUTS DE MA RECHERCHE</h3>
                 <div className={styles.allGraphs}>
-                    <Graph />
-                    <Histogram data={histogramData} />
-                    <div className={styles.details}>
-                        <p>Connectez-vous pour obtenir plus d’informations sur les statuts, les démarches aini que des comparaisons !</p>
-                        <Link href="/login">
-                        <a className={styles.ad}>Se connecter</a> 
-                        </Link>
-                    </div>
+                
+                <div className={styles.graphs}>
+                <Graph />
+                </div>
+                <div className={styles.statusRatio}>
+                    <Status/>
+                </div>
                 </div>
             </div>
         </div>
