be11ac686e43f2a2101d1bf1b69b4a8681cdb367
raphaelle <raflanelle@hotmail.com>
2024-08-14 10:24:09 +0200

    result comparaison des statuts

--- components/Result/Comparaisonstatus.js
+++ components/Result/Comparaisonstatus.js
@@ -307,54 +307,6 @@ if (!search.length) {
 
     return (
         <div className={styles.statusContainer}>
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Auto-Entrepreneur</th>
-                        <th>Travailleur indépendant</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>CA max/an</td>
-                        <td>75K BRUT</td>
-                        <td>45K BRUT</td>
-                    </tr>
-                    <tr>
-                        <td>Taux de cotisation</td>
-                        <td>25%</td>
-                        <td>70%</td>
-                    </tr>
-                    <tr>
-                        <td>Indemnités journalières</td>
-                        <td>✅</td>
-                        <td>❌</td>
-                    </tr>
-                    <tr>
-                        <td>Eligible a l'ACRE</td>
-                        <td>✅</td>
-                        <td>❌</td>
-                    </tr>
-                    <tr>
-                        <td>Nombre de salariés max</td>
-                        <td>❌</td>
-                        <td>✅</td>
-                    </tr>
-                    <tr>
-                        <td>Impot libératoire</td>
-                        <td>❌</td>
-                        <td>✅</td>
-                    </tr>
-                    <tr>
-                        <td>Droits chomage</td>
-                        <td>✅</td>
-                        <td>❌</td>
-                    </tr>
-                </tbody>
-            </table>
-            <a href="/allstatus">Acceder à tous les status</a>
-            <a href="/lastsearches">Accéder à toutes mes recherches</a>
         </div>
     );
 }
